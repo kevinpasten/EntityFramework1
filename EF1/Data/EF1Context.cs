77e9f1bcc63505dbe0d0f27138f79c0233fb3126
kevinpasten <kevin.pasten02@inacapmail.cl>
2023-06-22 20:45:25 -0400

    Agregar archivos de proyecto.

---
+++ EF1/Data/EF1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EF1.Models;
+
+namespace EF1.Data
+{
+    public class EF1Context : DbContext
+    {
+        public EF1Context (DbContextOptions<EF1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EF1.Models.Clients> Clients { get; set; } = default!;
+    }
+}
---
+++ EF1/Models/Clients.cs
@@ -0,0 +1,11 @@
+﻿namespace EF1.Models
+{
+    public class Clients
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+    }
+}
